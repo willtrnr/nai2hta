5a3e78d570b28a801060857585bb11ce9fcb5448
William Turner <willtur.will@gmail.com>
2022-10-16 02:04:21 -0400

    Add (shitty) support for the sd-webui PNG info as well

--- nai2hta.py
+++ nai2hta.py
@@ -13,12 +13,17 @@ from pathlib import Path
 
 from PIL import Image
 
+Tag = str | tuple[str, str]
+
 WS = re.compile(r"\s+")
 
 
 ## Matches the Hydrus Tag Archive schema
 HTA_SCHEMA = """
-    BEGIN;
+    PRAGMA automatic_index = OFF;
+    PRAGMA encoding = 'UTF-8';
+    PRAGMA page_size = 4096;
+
     CREATE TABLE hash_type ( hash_type INTEGER );
 
     CREATE TABLE hashes ( hash_id INTEGER PRIMARY KEY, hash BLOB_BYTES );
@@ -33,28 +38,41 @@ HTA_SCHEMA = """
     CREATE UNIQUE INDEX tags_tag_index ON tags ( tag );
 
     INSERT INTO hash_type (hash_type) VALUES (2);
-    COMMIT;
-"""
+    """
 
 
 class HTA:
+    __slots__ = (
+        "_path",
+        "_conn",
+        "_tags",
+        "_namespaces",
+        "_last_hash_id",
+        "_last_tag_id",
+    )
+
+    _path: Path | str
     _conn: sqlite3.Connection
 
-    _namespaces: set[str]
-
     _tags: dict[str, int]
+
+    _namespaces: set[str]
 
     _last_hash_id: int
     _last_tag_id: int
 
     def __init__(self, path: Path | str) -> None:
-        self._conn = sqlite3.connect(path)
-        self._tags = {}
-        self._last_tag_id = 0
-        self._last_hash_id = 0
+        self._path = path
+        self._connect()
         self._load()
 
+    def _connect(self) -> None:
+        self._conn = sqlite3.connect(self._path, isolation_level=None)
+        self._conn.execute("PRAGMA journal_mode = MEMORY")
+
     def _load(self) -> None:
+        self._tags = {}
+
         with contextlib.closing(self._conn.cursor()) as cur:
             try:
                 cur.execute("SELECT hash_type FROM hash_type")
@@ -85,11 +103,10 @@ class HTA:
                 "INSERT INTO hashes (hash_id, hash) VALUES (?, ?)",
                 (hash_id, hash_bytes),
             )
-            cur.connection.commit()
             self._last_hash_id = hash_id
             return hash_id
 
-    def _ensure_tag(self, tag: str | tuple[str, str]) -> int:
+    def _ensure_tag(self, tag: Tag) -> int:
         if isinstance(tag, tuple):
             ns, tag = tag
         else:
@@ -99,7 +116,6 @@ class HTA:
             if ns is not None:
                 if ns not in self._namespaces:
                     cur.execute("INSERT INTO namespaces (namespace) VALUES (?)", (ns,))
-                    cur.connection.commit()
                     self._namespaces.add(ns)
                 tag = f"{ns}:{tag}"
 
@@ -113,16 +129,14 @@ class HTA:
 
             tag_id = self._last_tag_id + 1
             cur.execute("INSERT INTO tags (tag_id, tag) VALUES (?, ?)", (tag_id, tag))
-            cur.connection.commit()
             self._last_tag_id = tag_id
             return tag_id
 
-    def add_tags(self, file_hash: str, tags: set[str | tuple[str, str]]) -> None:
+    def add_tags(self, file_hash: str, tags: set[Tag]) -> None:
         hash_id = self._ensure_hash(file_hash)
         mappings = {(hash_id, self._ensure_tag(t)) for t in tags}
         with contextlib.closing(self._conn.cursor()) as cur:
             cur.executemany("INSERT OR IGNORE INTO mappings VALUES (?, ?)", mappings)
-            cur.connection.commit()
 
     def close(self) -> None:
         self._conn.commit()
@@ -134,35 +148,75 @@ def parse_tags(tags: str) -> Iterable[str]:
         for tag in mixing.split(","):
             if ":" in tag:
                 tag = tag.split(":", 1)[0]
-            if tag := tag.strip("{}[], ").lower():
+            if tag := tag.strip("(){}[], ").lower():
                 yield tag
 
 
-def resolve_model(name: str) -> str:
-    name = name.split(" ")[-1]
-    if name == "81274D13":
+def identify_model(name: str) -> str:
+    name = name.split(" ")[-1].lower()
+    if name in ("81274d13", "925997e9"):
         return "full"
-    if name == "1D44365E":
+    if name in ("1d44365e", "1d4a34af"):
         return "curated"
-    return name.lower()
+    return name
 
 
-def derive_tags(image_path: Path | str) -> set[str | tuple[str, str]] | None:
+def derive_novel_ai_tags(info: dict[str, str]) -> Iterable[Tag]:
+    yield from parse_tags(info["Description"])
+
+    yield ("model", identify_model(info["Source"]))
+
+    params = json.loads(info["Comment"])
+    yield from (("uc", t) for t in parse_tags(params["uc"]))
+    for param in ("steps", "sampler", "seed", "scale", "noise", "strength"):
+        yield (param, str(params.get(param)).lower())
+
+
+def derive_sd_tags(info: str) -> Iterable[Tag]:
+    lines = info.split("\n")
+
+    if len(lines) < 2:
+        return
+
+    is_uc = False
+    for line in lines[:-1]:
+        if line.startswith("Negative prompt: "):
+            line = line[17:]
+            is_uc = True
+
+        if is_uc:
+            yield from (("uc", t) for t in parse_tags(line))
+        else:
+            yield from parse_tags(line)
+
+    for param in lines[-1].lower().split(","):
+        key, value = param.strip().split(": ", 1)
+
+        if key == "size":
+            continue
+
+        if key == "model hash":
+            key = "model"
+            value = identify_model(value)
+        elif key == "cfg scale":
+            key = "scale"
+        elif key == "sampler":
+            if value == "euler a":
+                value = "k_euler_ancestral"
+            elif value == "euler":
+                value = "k_euler"
+
+        yield (key, value)
+
+
+def derive_tags(image_path: Path | str) -> set[Tag] | None:
     try:
         with contextlib.closing(Image.open(image_path)) as im:
-            if im.info.get("Software") != "NovelAI":
-                return None
-
-            tags: set[str | tuple[str, str]] = set(parse_tags(im.info["Description"]))
-
-            tags.add(("model", resolve_model(im.info["Source"])))
-
-            params = json.loads(im.info["Comment"])
-            tags.update(("uc", t) for t in parse_tags(params["uc"]))
-            for param in ("steps", "sampler", "seed", "scale", "noise", "strength"):
-                tags.add((param, str(params.get(param)).lower()))
-
-            return tags
+            if im.info.get("Software") == "NovelAI":
+                return set(derive_novel_ai_tags(im.info))
+            if params := im.info.get("parameters"):
+                return set(derive_sd_tags(params))
+            return None
     except Exception as ex:
         print(ex)
         pass
